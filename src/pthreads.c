c90640fae5aeb54c9569558447d7f51355d0368a
Pedro Gonnet <gonnet@google.com>
2025-01-09 13:31:02 -0800

    Wrap `#define _GNU_SOURCE` in an `#ifndef` in case it has already been defined.
    PiperOrigin-RevId: 713777960

--- src/pthreads.c
+++ src/pthreads.c
@@ -8,7 +8,9 @@
 // LICENSE file in the root directory of this source tree.
 
 // Needed for syscall.
-#define _GNU_SOURCE
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif  // _GNU_SOURCE
 
 /* Standard C headers */
 #include <assert.h>
